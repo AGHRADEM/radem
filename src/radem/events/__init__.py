9cb0b7fb8767a213514b09c3c19680e798050bf4
Krzysztof Pęczek <peczek.kr@gmail.com>
2024-10-29 15:45:26 +0100

    Fixed an issue with imports

--- src/radem/events/__init__.py
+++ src/radem/events/__init__.py
@@ -1,4 +1,3 @@
 from .onset import detect_onset
-from .background import find_events, noise_mask, intervals_from_mask
 
-__all__ = ['detect_onset', 'find_events',  'noise_mask', 'intervals_from_mask']
+__all__ = ['detect_onset']

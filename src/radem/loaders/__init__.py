8efb2e4c82f68b7441730d3ed222dc5b826533ca
Krzysztof Pęczek <peczek.kr@gmail.com>
2024-10-29 17:05:44 +0100

    Added a `.cdf` loader

--- src/radem/loaders/__init__.py
+++ src/radem/loaders/__init__.py
@@ -1,3 +1,4 @@
 from .csv_loader import load_csv, save_csv
+from .cdf_loader import load_science_cdfs   
 
-__all__ = ['load_csv', 'save_csv']
+__all__ = ['load_csv', 'save_csv', 'load_science_cdfs']

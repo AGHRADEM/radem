d8a9912d2278a798bcc4b6fb7bfdb2132ee93f62
Szymon Bednorz <dsonyyy@gmail.com>
2024-12-11 23:02:39 +0100

    fix: radem data file handlers

--- src/radem/handlers/__init__.py
+++ src/radem/handlers/__init__.py
@@ -1,17 +1,59 @@
-from .irem_cdf_handler import get_irem_cdf_paths, \
-    read_irem_cdf, read_irem_cdfs, convert_irem_cdf_to_df, \
+from .csv_handler import (
+    write_csv,
+    read_csv
+)
+
+from .hdf_handler import (
+    write_hdf,
+    read_hdf,
+    append_hdf
+)
+
+from .irem_cdf_handler import (
+    get_irem_cdf_paths,
+    read_irem_cdf,
+    read_irem_cdfs,
+    convert_irem_cdf_to_df,
     convert_irem_cdfs_to_df
-from .hdf_handler import write_hdf, read_hdf, append_hdf
-from .csv_handler import write_csv, read_csv
+)
+
+from .radem_cdf_handler import (
+    get_radem_science_cdf_paths,
+    get_radem_housekeeping_cdf_paths,
+    read_radem_cdf,
+    read_radem_cdfs,
+    read_radem_science_cdfs,
+    read_radem_housekeeping_cdfs,
+    convert_radem_science_cdf_to_df,
+    convert_radem_housekeeping_cdf_to_df,
+    convert_radem_science_cdfs_to_df
+)
 
 
-__all__ = ['get_irem_cdf_paths',
-           'read_irem_cdf',
-           'read_irem_cdfs',
-           'convert_irem_cdf_to_df',
-           'convert_irem_cdfs_to_df',
-           'write_hdf',
-           'read_hdf',
-           'append_hdf',
-           'write_csv',
-           'read_csv']
+__all__ = [
+    # irem_cdf_handler
+    'get_irem_cdf_paths',
+    'read_irem_cdf',
+    'read_irem_cdfs',
+    'convert_irem_cdf_to_df',
+    'convert_irem_cdfs_to_df',
+
+    # radem_cdf_handler
+    "get_radem_science_cdf_paths",
+    "get_radem_housekeeping_cdf_paths",
+    "read_radem_cdf",
+    "read_radem_cdfs",
+    "read_radem_science_cdfs",
+    "read_radem_housekeeping_cdfs",
+    "convert_radem_science_cdf_to_df",
+    "convert_radem_housekeeping_cdf_to_df",
+    "convert_radem_science_cdfs_to_df",
+
+    # hdf_handler
+    'write_hdf',
+    'read_hdf',
+    'append_hdf',
+
+    # csv_handler
+    'write_csv',
+    'read_csv']
